8137b10fdcd270ff3318629fe6ea6c550e07a244
Alexandre Radin <alexandre.radin@isen-ouest.yncrea.fr>
2025-02-06 09:17:09 +0100

    class Lieu implemented

--- tsp_graph_init.py
+++ tsp_graph_init.py
@@ -1,1 +1,10 @@
-# hello 
+import numpy as np
+
+class Lieu:
+    def __init__(self, x, y, nom):
+        self.x = x
+        self.y = y
+        self.nom = nom
+
+    def distance(self, lieu):
+        return np.sqrt((self.x - lieu.x)**2+(self.y - lieu.y)**2)
