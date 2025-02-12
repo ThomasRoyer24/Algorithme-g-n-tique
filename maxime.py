192a84c88b83c3d6b64f0d400d36d5f68834e6ff
Alexandre Radin <alexandre.radin@isen-ouest.yncrea.fr>
2025-02-12 16:12:17 +0100

    best route --> n best routes

--- maxime.py
+++ maxime.py
@@ -77,6 +77,92 @@ class Route:
             self.ordre = ordre[:]
     
 
+
+class TSP_GA:
+    def __init__(self, graph, taille_population=50, prob_croisement=0.8, prob_mutation=0.05, ratio_selection=0.7):
+        self.population = []
+        self.graph = graph
+        self.taille_population = taille_population
+        self.prob_croisement = prob_croisement
+        self.prob_mutation = prob_mutation
+        self.ratio_selection = ratio_selection
+
+    def initialiser_population(self):
+        for _ in range(self.taille_population):
+            route = Route(None)
+            self.population.append(route)
+
+    def selection(self):
+        # selection proportionnelle à l'adaptation
+        # distances = []
+        # for route in self.population:
+        #     distance = self.graph.calcul_distance_route(route)
+        #     distances.append(distance)
+        # # probas = [distance/total_distance for distance in distances]
+        # probas = [1 / distance for distance in distances]
+        # total_prob = sum(probas)
+        # probas = [p / total_prob for p in probas]
+        # return np.random.choice(self.population, int(self.ratio_selection*self.taille_population), p=probas).tolist()
+
+        # selection par tournoi
+        selectionnes = []
+        for _ in range(int(self.ratio_selection * self.taille_population)):
+            tournoi = np.random.choice(self.population, 3)  # Sélection de 3 individus
+            meilleur_individu = min(tournoi, key=lambda x: self.graph.calcul_distance_route(x))
+            selectionnes.append(meilleur_individu)
+        return selectionnes
+
+    def croisement(self, parent1, parent2):
+        enfants = []
+
+        cut = np.random.randint(1, NB_LIEUX-1)
+
+        enfant1 = Route(parent1.ordre[:cut] + [x for x in parent2.ordre if x not in parent1.ordre[:cut]] + [0])
+        enfant2 = Route(parent2.ordre[:cut] + [x for x in parent1.ordre if x not in parent2.ordre[:cut]] + [0])
+        
+        enfants.append(enfant1)
+        enfants.append(enfant2)
+        
+        return enfants
+
+    def mutation(self, enfant):       
+        villes_a_echanger = np.random.default_rng().choice(range(1, NB_LIEUX-1), size=2, replace=False)
+        enfant.ordre[villes_a_echanger[0]], enfant.ordre[villes_a_echanger[1]] = enfant.ordre[villes_a_echanger[1]], enfant.ordre[villes_a_echanger[0]]
+        return enfant
+    
+    def test(self):
+        best = np.inf
+        best_index = -1
+        for idx, route in enumerate(self.population):
+            distance = self.graph.calcul_distance_route(route)
+            if distance < best:
+                best = distance
+                best_index = idx
+        print(f"Meilleure distance: {best}")
+        print(f"Indice de l'individu avec la plus petite distance: {best_index}")
+        return best_index
+
+    def run(self):
+        self.initialiser_population()
+        _ = self.test()
+        selectionnes = self.selection()
+        nouvelle_population = []
+        for j in range(0, len(selectionnes)-1, 2):
+            if np.random.rand() < self.prob_croisement:
+                enfants = self.croisement(selectionnes[j], selectionnes[j+1])
+                for enfant in enfants:
+                    if np.random.rand() < self.prob_mutation:
+                        enfant = self.mutation(enfant)
+                    nouvelle_population.append(enfant)
+            else:
+                nouvelle_population.append(selectionnes[j])
+                nouvelle_population.append(selectionnes[j+1])
+                        
+        self.population = nouvelle_population
+        print(len(self.population))
+        return self.population, self.test()
+
+
 class Affichage:
     def __init__(self, graph):
         self.graph = graph
@@ -88,13 +174,25 @@ class Affichage:
         self.label = tk.Label(self.window, text="Informations:")
         self.label.pack()
 
+        self.paused = False
+        self.show_best_routes = True
+        self.window.bind("<space>", self.toggle_pause)
+        self.window.bind("<b>", self.toggle_best_routes)
         self.window.bind("<Key>", self.handle_key_press)
         self.window.bind("<Escape>", self.handle_escape)
+
+    def toggle_best_routes(self, event):
+        self.show_best_routes = not self.show_best_routes
+        if not self.show_best_routes:
+            self.canvas.delete("route")
 
     def update_label(self, text):
         self.label.config(text=text)
         #self.text = tk.Text(self.window)
         #self.text.pack()
+
+    def toggle_pause(self, event):
+        self.paused = not self.paused
 
     def handle_key_press(self, event):
         # if event.char == "N":
@@ -104,17 +202,20 @@ class Affichage:
         self.window.destroy()
 
     def display_n_best_routes(self, population, n=5):
-        self.canvas.delete("route")
-        for route in population:
-            self.display_route(route, color="gray")
+        if self.show_best_routes:
+            self.canvas.delete("route")
+            for route in population:
+                self.display_route(route, color="gray")
 
     def display_route(self, route, color, mini=False):
+        if mini:
+                self.canvas.delete("route_mini")
         for i in range(len(route.ordre) - 1):
+            
             lieu_actuel = self.graph.liste_lieux[route.ordre[i]]
             lieu_suivant = self.graph.liste_lieux[route.ordre[i + 1]]
 
             if mini:
-                self.canvas.delete("route_mini")
                 self.canvas.create_line(lieu_actuel.x, lieu_actuel.y, lieu_suivant.x, lieu_suivant.y, fill="blue", tag="route_mini",width=5)
             else:
                 self.canvas.create_line(lieu_actuel.x, lieu_actuel.y, lieu_suivant.x, lieu_suivant.y, fill=color, dash=(4, 4), tag="route")
@@ -124,12 +225,6 @@ class Affichage:
         self.window.mainloop()
 
 
-#test
-graph = Graph()
-NB_LIEUX = graph.generer_lieux(path='data/graph_5.csv')
-# print("NB_lieux : ",NB_LIEUX)
-graph.calcul_matrice_cout_od()
-# print("Matrice od : \n",graph.matrice_od)
 
 #test du plus proche voisin
 # lieu = graph.liste_lieux[1]
@@ -140,34 +235,31 @@ graph.calcul_matrice_cout_od()
 # route = Route([0, 3 ,1 ,4 ,2, 0])
 # print("Distance total:", graph.calcul_distance_route(route))
 
-# #test affichage
-affichage = Affichage(graph)
 # best_route = Route(None)
 # print(best_route.ordre)
 # print("Distance total:", graph.calcul_distance_route(best_route))
-# best_route = Route(None)
-# print(best_route.ordre)
-# print("Distance total:", graph.calcul_distance_route(best_route))
+
 
 # main
-nb_population = 5
-nb_iterations = 1000
-population = list(Route(None) for _ in range(nb_population))
+graph = Graph()
+NB_LIEUX = graph.generer_lieux(path='data/graph_200.csv')
+# print("NB_lieux : ",NB_LIEUX)
+graph.calcul_matrice_cout_od()
+affichage = Affichage(graph)
+nb_iterations = 2000
 
-
-affichage.display_n_best_routes(population)
-indice_minimum_distance = random.randint(0, nb_population-1)
-affichage.display_route(route=population[indice_minimum_distance], color="blue", mini=True)
+nb_population = 50
 
 for i in range(nb_iterations):
 
-    # algo alex
-    # population = new_population ??
-    population = list(Route(None) for _ in range(nb_population))
-    indice_minimum_distance = random.randint(0, nb_population-1)
+    while affichage.paused:
+        affichage.window.update_idletasks()
+        affichage.window.update()
+
+    solver = TSP_GA(graph, taille_population=nb_population, prob_mutation=0.5)
+    population, indice_minimum_distance = solver.run()
 
     affichage.display_route(route=population[indice_minimum_distance], color="blue", mini=True)
-    print(population[indice_minimum_distance])
     affichage.display_n_best_routes(population)
     affichage.update_label("Iteration: " + str(i) + " Meilleur distance trouvée : " + str(graph.calcul_distance_route(population[indice_minimum_distance])))
 
@@ -175,87 +267,5 @@ for i in range(nb_iterations):
     affichage.window.update()
 
 affichage.run()
-
-
-
-class TSP_GA:
-    def __init__(self):
-        self.population = []
-        self.graph = Graph()
-        self.graph.generer_lieux()
-        self.graph.calcul_matrice_cout_od()
-        self.taille_population = 5
-        self.nb_generations = 10000
-        self.prob_croisement = 0.8
-        self.prob_mutation = 0.05
-        self.ratio_selection = 0.7
-
-    def initialiser_population(self):
-        for _ in range(self.taille_population):
-            route = Route(None)
-            self.population.append(route)
-        print("Population initiale: ",self.population)
-
-
-    def selection(self):
-        # selection proportionnelle à l'adaptation
-        distances = []
-        total_distance = 0
-        for route in self.population:
-            distance = self.graph.calcul_distance_route(route)
-            distances.append(distance)
-            total_distance += distance
-        probas = [distance/total_distance for distance in distances]
-        return np.random.choice(self.population, int(self.ratio_selection*self.taille_population), p=probas).tolist()
-
-    def croisement(self, selectionnes):
-        enfants = []
-        for i in range(0, len(selectionnes)-1, 2):
-            parent1 = selectionnes[i]
-            parent2 = selectionnes[i+1]
-            cut = np.random.randint(1, NB_LIEUX-1)
-
-            enfant1 = Route(parent1.ordre[:cut] + [x for x in parent2.ordre if x not in parent1.ordre[:cut]] + [0])
-            enfant2 = Route(parent2.ordre[:cut] + [x for x in parent1.ordre if x not in parent2.ordre[:cut]] + [0])
-            enfants.append(enfant1)
-            enfants.append(enfant2)
-        return enfants
-
-    def mutation(self, enfants):
-        enfants_mutes = []
-        for enfant in enfants:
-            if np.random.rand() < self.prob_mutation:
-                villes_a_echanger = np.random.default_rng().choice(range(1, NB_LIEUX-1), size=2, replace=False)
-                enfant.ordre[villes_a_echanger[0]], enfant.ordre[villes_a_echanger[1]] = enfant.ordre[villes_a_echanger[1]], enfant.ordre[villes_a_echanger[0]]
-                enfants_mutes.append(enfant)
-        return enfants_mutes
-    
-    def evaluate(self):
-        best = np.inf
-        for individu in self.population:
-            if self.graph.calcul_distance_route(individu) < best:
-                best = self.graph.calcul_distance_route(individu)
-        print("Meilleure distance: ",best)
-
-    def run(self):
-        self.initialiser_population()
-        self.evaluate()
-        print("-----")
-        for i in range(self.nb_generations):
-            selectionnes = self.selection()
-            enfants = self.croisement(selectionnes)
-            enfants_mutes = self.mutation(enfants)
-            best_individuals = sorted(self.population, key=lambda x: self.graph.calcul_distance_route(x))[:int(self.ratio_selection * self.taille_population)]
-            new_individuals = selectionnes + enfants_mutes
-            self.population = best_individuals + new_individuals[:len(self.population) - len(best_individuals)]
-
-        self.evaluate()
-            
-
-#solver = TSP_GA()
-#solver.run()
-
-
-
 
 
